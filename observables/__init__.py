a9c23c967f1f2ee1878bc265f010c2a243207439
bab <benedikt.brandes@me.com>
2025-08-12 13:31:21 +0200

    version system patch

--- observables/__init__.py
+++ observables/__init__.py
@@ -83,8 +83,8 @@ __all__ = [
 try:
     from ._version import __version__, __version_tuple__
 except ImportError:
-    __version__ = "0.2.4"
-    __version_tuple__ = (0, 2, 4)
+    __version__ = "0.2.5"
+    __version_tuple__ = (0, 2, 5)
 
 __author__ = 'Benedikt Axel Brandes'
 __year__ = '2025'

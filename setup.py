0de7121885e8edb34680af20da3694fba0f44342
bab <benedikt.brandes@me.com>
2025-08-15 20:38:57 +0200

    feat: Empty

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read_version():
                 if line.startswith("__version__"):
                     return line.split("=")[1].strip().strip('"\'')
     except FileNotFoundError:
-        return "0.3.1"  # fallback version
+        return "0.4.0"  # fallback version
 
 setup(
     name="observables",

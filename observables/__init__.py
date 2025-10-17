90d2781a2b97d8e666636267f38c2eb574391d30
bab <benedikt.brandes@me.com>
2025-10-17 22:44:15 +0200

    hiding module path of submission error

--- observables/__init__.py
+++ observables/__init__.py
@@ -166,8 +166,8 @@ __all__ = [
 try:
     from ._version import __version__, __version_tuple__
 except ImportError:
-    __version__ = "4.1.16"
-    __version_tuple__ = (4, 1, 16)
+    __version__ = "4.1.17"
+    __version_tuple__ = (4, 1, 17)
 
 __author__ = 'Benedikt Axel Brandes'
 __year__ = '2025'
